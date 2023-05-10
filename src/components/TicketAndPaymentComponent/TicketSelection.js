70ab66441e9928485d42cca1505384321587ab1e
Eduardo Souza <edcampossouza@gmail.com>
2023-05-10 00:06:37 -0300

    feat: select with or without accomodation

--- src/components/TicketAndPaymentComponent/TicketSelection.js
+++ src/components/TicketAndPaymentComponent/TicketSelection.js
@@ -1,33 +1,79 @@
 import styled from 'styled-components';
 import useTicketTypes from '../../hooks/api/useTicketTypes';
+import { useState } from 'react';
 
 export default function TicketSelection() {
   const { ticketTypes } = useTicketTypes();
+
+  const [selectedInPerson, setSelectedInPerson] = useState(false);
+  const [selectedRemote, setSelectedRemote] = useState(false);
+  const [selectWithHotel, setSelectedWithHotel] = useState(false);
+  const [selectWithoutHotel, setSelectedWithoutHotel] = useState(false);
+
+  const remoteTicket = ticketTypes?.find((t) => t.isRemote);
+  const inPersonTicket = ticketTypes?.find((t) => !t.isRemote && !t.includesHotel);
+  const inPersonWithHotelTicket = ticketTypes?.find((t) => !t.isRemote && t.includesHotel);
+
   if (!ticketTypes || ticketTypes.length < 1) {
     return 'Os preços dos ingressos ainda não foram cadastrados ';
   }
-  const remoteTicket = ticketTypes.find((t) => t.isRemote);
-  const inPersonTicket = ticketTypes.find((t) => !t.isRemote && !t.includesHotel);
-  const inPersonWithHotelTicket = ticketTypes.find((t) => !t.isRemote && t.includesHotel);
 
   return (
     <>
       <>
         <RowTitle>Primeiro, escolha sua modalidade de ingresso</RowTitle>
         <TicketsRow>
-          <TicketPriceContainer selected={inPersonTicket.selected}>
+          <TicketPriceContainer
+            selected={selectedInPerson}
+            onClick={() => {
+              setSelectedInPerson(true);
+              setSelectedRemote(false);
+            }}
+          >
             <span className="ticket-type-name">Presencial</span>
-            <span className="ticket-price"> {inPersonTicket.price}</span>
+            <span className="ticket-price"> {`R$ ${inPersonTicket.price}`}</span>
           </TicketPriceContainer>
-          <TicketPriceContainer>
-            <span className="ticket-type-name" selected={remoteTicket.selected}>
-              Online
-            </span>
-            <span className="ticket-price"> {remoteTicket.price}</span>
+          <TicketPriceContainer
+            selected={selectedRemote}
+            onClick={() => {
+              setSelectedRemote(true);
+              setSelectedInPerson(false);
+            }}
+          >
+            <span className="ticket-type-name">Online</span>
+            <span className="ticket-price"> {`R$ ${remoteTicket.price}`}</span>
           </TicketPriceContainer>
         </TicketsRow>
-        {JSON.stringify(ticketTypes)}
       </>
+      {selectedInPerson && (
+        <>
+          <RowTitle>Ótimo! Agora escolha sua modalidade de hospedagem</RowTitle>
+          <TicketsRow>
+            <TicketPriceContainer
+              selected={selectWithoutHotel}
+              onClick={() => {
+                setSelectedWithHotel(false);
+                setSelectedWithoutHotel(true);
+              }}
+            >
+              <span className="ticket-type-name">Sem Hotel</span>
+              <span className="ticket-price"> + R$ 0</span>
+            </TicketPriceContainer>
+            {inPersonWithHotelTicket && (
+              <TicketPriceContainer
+                selected={selectWithHotel}
+                onClick={() => {
+                  setSelectedWithHotel(true);
+                  setSelectedWithoutHotel(false);
+                }}
+              >
+                <span className="ticket-type-name">Online</span>
+                <span className="ticket-price"> {`+ R$ ${inPersonWithHotelTicket.price - inPersonTicket.price}`}</span>
+              </TicketPriceContainer>
+            )}
+          </TicketsRow>
+        </>
+      )}
     </>
   );
 }
@@ -50,9 +96,6 @@ const TicketPriceContainer = styled.div`
   .ticket-price {
     font-size: 14px;
     color: #898989;
-    :before {
-      content: 'R$ ';
-    }
   }
 `;
 
